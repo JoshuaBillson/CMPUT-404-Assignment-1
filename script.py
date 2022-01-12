1368bbd2b4b201ecd9889bd66dc7d0e0800f6fdb
JoshuaBillson <jmbillson@outlook.com>
2022-01-12 00:00:35 -0700

    Modified Script

--- script.py
+++ script.py
@@ -2,8 +2,14 @@ import requests
 
 
 def main():
+    print(f"Requests Version: {requests.__version__}")
+
+    google_result = requests.get("http://www.google.com")
+    print(f"\nRequest to http://www.google.com returned code {google_result.status_code}")
+
     result = requests.get("https://raw.githubusercontent.com/JoshuaBillson/CMPUT-404-Assignment-1/main/script.py")
-    print(result.text)
+
+    print(f"\n{result.text}")
 
 
 if __name__ == "__main__":
